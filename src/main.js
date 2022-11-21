8fa5c2fe783bc139fb6105fde723ac9b8389f7ec
a.vinsonneau <antoine.vinsonneau@compufirst.com>
2022-11-21 15:50:40 +0100

    Ajout interfaceIHM

---
+++ src/main.js
@@ -0,0 +1,2 @@
+const Calculator = require("../src/calculator").Calculator;
+const expect = require("chai").expect;
--- test/calculator.test.js
+++ test/calculator.test.js
@@ -1,5 +1,3 @@
-const calculator = require("../src/calculator");
-
 const Calculator = require("../src/calculator").Calculator;
 const expect = require("chai").expect;
 
