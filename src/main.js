ac2736fa63ddae305b46cac44735018fc2695129
AntoineP2 <antoine.vinsonneau34@gmail.com>
2022-11-22 11:21:02 +0100

    Main ok

--- src/main.js
+++ src/main.js
@@ -1,22 +1,29 @@
+const CalculatorScientist = require("../src/calculatorScientist").CalculatorScientist;
 const Calculator = require("../src/calculator").Calculator;
 const prompt = require("prompt-sync")({ sigint: true });
 
 const calculator = new Calculator();
+const calculatorScientist = new CalculatorScientist()
 let statue = 1
+let var1 = 0
+let var2 = 0
 while (statue == 1) {
     console.log("1 = Addition");
     console.log("2 = Substraction");
     console.log("3 = Multplication");
     console.log("4 = Division");
+    console.log("5 = Pow");
+    console.log("6 = Simple Pow");
+    console.log("7 = Square Root");
     let calculeChoice = Number(prompt("Choose a calcule type: "));
-    if (Number <=5){
-        let var1 = Number(prompt("Choose a value: "));
-        let var2 = Number(prompt("Choose a another value : "));
+    
+    if (calculeChoice <= 5) {
+        var1 = Number(prompt("Choose a value: "));
+        var2 = Number(prompt("Choose a another value : "));
     }
-    else{
-        let var1 = Number(prompt("Choose a value: "));
+    else {
+        var1 = Number(prompt("Choose a value: "));
     }
-
     switch (calculeChoice) {
         case 1:
             console.log(calculator.addition(var1, var2))
@@ -29,6 +36,15 @@ while (statue == 1) {
             break
         case 4:
             console.log(calculator.division(var1, var2))
+            break
+        case 5:
+            console.log(calculatorScientist.pow(var1, var2))
+            break
+        case 6:
+            console.log(calculatorScientist.simplePow(var1))
+            break
+        case 7:
+            console.log(calculatorScientist.squareRoot(var1))
             break
         default:
             console.log("Veuillez rentrer une valeur en convenance avec ce qui vous ai demandé")
