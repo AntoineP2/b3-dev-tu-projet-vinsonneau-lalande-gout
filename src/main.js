0dce155550eb4952c3e32c1c48514fbb54aefacd
a.vinsonneau <antoine.vinsonneau@compufirst.com>
2022-11-22 10:40:58 +0100

    modif

--- src/main.js
+++ src/main.js
@@ -1,2 +1,11 @@
 const Calculator = require("../src/calculator").Calculator;
-const expect = require("chai").expect;
+const prompt = require("prompt-sync")({ sigint: true });
+
+const calculator = new Calculator();
+let var1 = Number(prompt("Choose a value: "));
+let var2 = Number(prompt("Choose a another valuer : "));
+console.log("1 = Addition");
+console.log("2 = Substraction");
+console.log("3 = Multplication");
+console.log("4 = Division");
+let calculeChoice = Number(prompt("Choose a calcule type: "));
